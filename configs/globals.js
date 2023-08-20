2fbbdb6ec3dee3e4d60c1edf2af46e551e07386f
jap545 <113646201+jap545@users.noreply.github.com>
2023-08-20 16:38:29 -0400

    Add files via upload

--- configs/globals.js
+++ configs/globals.js
@@ -6,13 +6,13 @@ const configurations = {
     "github": {
         "clientId": "f967b01290bbd43edeaf",
         "clientSecret": "9d58036e737333c0fdc696e2f81959e51e7cb039",
-        "callbackUrl": "http://localhost:3000/github/callback",
+        "callbackUrl": "https://user-management-s2023.onrender.com/github/callback",
     },
     // Google OAuth configuration
   google: {
     clientId: "946733545729-9n9qlienr0vsgjcc6tgnpv9s035t44cv.apps.googleusercontent.com",
     clientSecret: "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",
-    callbackUrl: "http://localhost:3000/auth/google/callback",
+    callbackUrl: "https://user-management-s2023.onrender.com/auth/google/callback",
   },
 }
 // export to make it available to the other files
